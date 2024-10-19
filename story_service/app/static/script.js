01feb3c41eb65f55b0ff7036826ac562d06d0a97
Matvey <seeyouantee@gmail.com>
2024-10-20 00:46:17 +0300

    WIP: fixed 'copy' button. Added clearing 'output' when new story arrives

--- story_service/app/static/script.js
+++ story_service/app/static/script.js
@@ -1,23 +1,31 @@
 const socket = new WebSocket('ws://localhost:8001/api/generate_story');
 const output = document.getElementById('output');
+const copyBtn = document.getElementById('copy-btn');
 
 socket.onmessage = function(event) {
     const chunk = event.data;
     output.innerHTML += chunk;
-    output.innerHTML += '<button class="copy-btn" onclick="copyToClipboard()">Копировать</button>';
+    copyBtn.style.display = 'block'; // Displaying 'Copy' button
+};
+
+socket.onclose = function(event) {
+    if (output.innerHTML.trim() !== '') {
+        copyBtn.style.display = 'block'; // Displaying 'Copy' button
+    }
 };
 
 async function generateStory() {
     const characters = document.getElementById('characters').value;
+    output.innerHTML = "";
+    output.textContent.replaceAll("");
     socket.send(JSON.stringify({ characters: characters }));
 }
 
 function copyToClipboard() {
-    const output = document.getElementById('output');
-    const textToCopy = output.textContent.replace('Копировать', '').trim();
+    const textToCopy = output.textContent.trim();
     navigator.clipboard.writeText(textToCopy).then(() => {
-        alert('Результат скопирован в буфер обмена!');
+        alert('Result copied to clipboard!');
     }, (err) => {
-        console.error('Не удалось скопировать текст: ', err);
+        console.error('Failed to copy text: ', err);
     });
 }
